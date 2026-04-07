398cd720d6e612d1439f2dc98bd28d333ce56805
poupardm-GhostWrath <mathpoupard@gmail.com>
2026-04-07 14:29:44 -0600

    List Users Lesson

--- handler_user.go
+++ handler_user.go
@@ -59,3 +59,19 @@ func printUser(user database.User) {
 	fmt.Printf(" * ID:		%v\n", user.ID)
 	fmt.Printf(" * Name:	%v\n", user.Name)
 }
+
+func handlerListUsers(s *state, cmd command) error {
+	users, err := s.db.GetUsers(context.Background())
+	if err != nil {
+		return fmt.Errorf("couldn't list users: %w", err)
+	}
+
+	for _, user := range users {
+		if user.Name == s.cfg.CurrentUserName {
+			fmt.Printf(" * %v (current)\n", user.Name)
+		} else {
+			fmt.Printf(" * %v\n", user.Name)
+		}
+	}
+	return nil
+}
