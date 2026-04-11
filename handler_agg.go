870cbc2f5afb0720488e0c291fb7790562113b94
poupardm-GhostWrath <mathpoupard@gmail.com>
2026-04-10 19:59:52 -0600

    Aggregate Lesson

--- handler_agg.go
+++ handler_agg.go
@@ -1,16 +1,58 @@
 package main
 
 import (
-	"context"
 	"fmt"
+	"time"
+	"log"
+	"context"
+
+	"github.com/poupardm-GhostWrath/gator/internal/database"
 )
 
 func handlerAgg(s *state, cmd command) error {
-	url := "https://www.wagslane.dev/index.xml"
-	feed, err := fetchFeed(context.Background(), url)
-	if err != nil {
-		return fmt.Errorf("couldn't get rss feed: %w", err)
+	if len(cmd.Args) < 1 || len(cmd.Args) > 2 {
+		return fmt.Errorf("usage: %v <time_between_reqs>", cmd.Name)
 	}
-	fmt.Printf("Feed: %+v\n", feed)
+
+	timeBetweenRequests, err := time.ParseDuration(cmd.Args[0])
+	if err != nil {
+		return fmt.Errorf("invalid duration: %w", err)
+	}
+	log.Printf("Collecting feeds every %s...\n", timeBetweenRequests.String())
+	ticker := time.NewTicker(timeBetweenRequests)
+	
+	for ; ; <-ticker.C {
+		scrapeFeeds(s)
+	}
 	return nil
+}
+
+func scrapeFeeds(s *state) {
+	// Get Next Feed
+	feed, err := s.db.GetNextFeedToFetch(context.Background())
+	if err != nil {
+		log.Printf("couldn't get next feed: %w", err)
+		return
+	}
+	log.Println("Found a feed to fetch!")
+	scrapeFeed(s.db, feed)
+}
+
+func scrapeFeed(db *database.Queries, feed database.Feed) {
+	_, err := db.MarkFeedFetched(context.Background(), feed.ID)
+	if err != nil {
+		log.Printf("couldn't mark feed %s fetched: %v", feed.Name, err)
+		return
+	}
+
+	rssFeed, err := fetchFeed(context.Background(), feed.Url)
+	if err != nil {
+		log.Printf("couldn't collect feed %s: %v", feed.Name, err)
+		return
+	}
+
+	for _, item := range rssFeed.Channel.Item {
+		fmt.Printf("Found post: %s\n", item.Title)
+	}
+	log.Printf("Feed %s collected, %v posts found", feed.Name, len(rssFeed.Channel.Item))
 }
